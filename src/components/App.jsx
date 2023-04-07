e7df3932562a1074b8b3e3ebec9a2aa82b833476
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-04-07 07:32:38 +0300

    add header with navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,18 @@
 import { HomePage } from 'pages/HomePage';
 import { MovieCard } from 'pages/MovieCard';
-import { Route, Routes } from 'react-router-dom';
+import { NavLink, Route, Routes } from 'react-router-dom';
 import { MovieCast } from './MovieCast/MovieCast';
 import { MovieReview } from './MovieReview/MovieReview';
 import { MoviePage } from 'pages/MoviePage';
+import { NotFound } from 'pages/NotFound';
 
 export const App = () => {
   return (
     <div>
+      <header>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/movies'>Movies</NavLink>
+      </header>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movies/:movieId" element={<MovieCard />}>
@@ -15,6 +20,7 @@ export const App = () => {
           <Route path="reviews" element={<MovieReview />} />
         </Route>
         <Route path="/movies" element={<MoviePage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+export const NotFound = () => {
+  return (
+    <>
+      <h1>Error! Please check if url address is correct.</h1>
+      <p>try this button to go back on home page</p>
+      <Link to={'/'}>
+        <button>Home page</button>
+      </Link>
+    </>
+  );
+};
